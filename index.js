fe4e4b567f2a728971b101d3dc1303ba119d40a6
Jessekay <kayjes00@gmail.com>
2026-08-19 11:14:01 +0200

    Throw an error for failed API fetch instead of logging a message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 fetch("https://pokeapi.co/api/v2/pokemon/spongebob")
 .then(response => {
   if(!response.ok) {
-    console.log('Could not fetch the resource');
+    throw new Error('Could not fetch the resource')
   }
 
   return response.json();
