c8f804c49d5db68de9273a286af144d85d3371d5
Jessekay <kayjes00@gmail.com>
2026-08-19 14:59:00 +0200

    Fix Pokémon sprite image source and improve error handling in fetchData function

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 fetchData();
 async function fetchData() {
   try {
-
-    const pokemonName = document.getElementById('pokemonName').value.toLowerCase;
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/${pokemonName}");
+    const inputElement = document.getElementById("pokemonName");
+    const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
     if(!response.ok) {
-      throw new error('Could not fetch the resource');
+      throw new Error('Could not fetch the resource');
     }
     const data = await response.json();
-    console.log(data);
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("pokemonSprite");
+    imgElement.src = pokemonSprite;
+    imgElement.style.display= "block";
+
+    inputElement.value = '';
     
   } catch (error) {
     console.error(error)
