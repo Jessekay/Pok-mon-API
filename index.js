75997768386f719222d392b2cb2fcaa65a494d39
Jessekay <kayjes00@gmail.com>
2026-08-19 11:00:35 +0200

    Update Pokémon API fetch to handle response errors for improved reliability

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-.then(response => response.json())
+fetch("https://pokeapi.co/api/v2/pokemon/spongebob")
+.then(response => {
+  if(!response.ok) {
+    console.log('Could not fetch the resource');
+  }
+
+  return response.json();
+})
 .then((data) => console.log(data.name))
 .catch(error => console.error(error))
